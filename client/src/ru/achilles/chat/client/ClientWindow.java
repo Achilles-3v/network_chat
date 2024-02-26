b7dbb6353dd74b5758c3ab2e72b5e76ed8dd1e8d
Achilles-3v <sh17vv@gmail.com>
2024-02-26 22:14:19 +0500

    ClientWindow added method main

--- client/src/ru/achilles/chat/client/ClientWindow.java
+++ client/src/ru/achilles/chat/client/ClientWindow.java
@@ -1,8 +1,19 @@
 package ru.achilles.chat.client;
 
-public class ClientWindow {
+import javax.swing.*;
+
+public class ClientWindow extends JFrame {
 
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientWindow();
+            }
+        });
+    }
+
+    private ClientWindow() {
 
     }
 }
