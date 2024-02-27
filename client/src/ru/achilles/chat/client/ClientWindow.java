3dd07dcee56ed07cbb4e90668451987930767944
Achilles-3v <sh17vv@gmail.com>
2024-02-27 14:19:21 +0500

    create window

--- client/src/ru/achilles/chat/client/ClientWindow.java
+++ client/src/ru/achilles/chat/client/ClientWindow.java
@@ -4,6 +4,11 @@ import javax.swing.*;
 
 public class ClientWindow extends JFrame {
 
+    private static final String IP_ADDR = "127.0.0.1";
+    private static final int PORT = 8182;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 400;
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -14,6 +19,11 @@ public class ClientWindow extends JFrame {
     }
 
     private ClientWindow() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
 
+        setVisible(true);
     }
 }
